c92064d1ab0082cdb15c0b455e371317e4a097b3
Ibrahim Ali <hemamessi47@gmail.com>
2021-07-20 01:18:57 +0200

    add delete button to posts

--- controllers/posts/deletePost.php
+++ controllers/posts/deletePost.php
@@ -1,12 +1,15 @@
 <?php 
 
 include "../../models/posts/deletePost.php";
+session_start();
 
 if(isset($_POST['deletePost'])){
-  $deletePosts = new DeletePostModel($_POST['delete']);
-  $allPosts = $deletePosts->delete_posts($id,$_user_id);
-  if($allPosts){
-    header('Location:../../views/posts/profile.php');
-  }
+  $deletePosts = new DeletePostModel($_POST['delete'] , $_SESSION['id']);
+  $deletePosts->delete_posts();
+  // if($allPosts){
+    header('Location:/friends/views/profile/profile.php');
+  // }
+}else{
+  echo 'error';
 }
 ?>
--- models/posts/deletePost.php
+++ models/posts/deletePost.php
@@ -4,18 +4,25 @@ require_once('/xampp/htdocs/friends/models/database.php');
 
   class DeletePostModel {
 
-    private $id;
+    private $post_id;
     private $user_id;
 
-    public function __construct()
+    public function __construct($post_id , $user_id)
     {
-        $this->db = new Database();
+      $this->post_id = $post_id;
+      $this->user_id = $user_id;
+      $this->db = new Database();
     }
-    public function delete_posts($id,$user_id){
-      $this->db->query("delete from post where id = :id and user_id = :user_id");
-      $this->db->bind(':id' , $id);
-      $this->db->bind(':user_id' , $user_id);
-      $this->db->execute();
+    public function delete_posts(){
+      $this->db->query("delete from post where post.id = :id and user_id=:user_id");
+      $this->db->bind(':id' , $this->post_id);
+      $this->db->bind(':user_id' , $this->user_id);
+      $deleted = $this->db->resultset();
+      if($deleted){
+        return true;
+      }else{
+        return false;
+      }
   }
 
 
